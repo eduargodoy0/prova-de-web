4b7c5ba36bb66c2ef18ea0242daad48b6f6965b7
eduargodoy0 <eduargodoy22@gmail.com>
2025-06-08 15:50:57 -0300

    style: adicionado estilo ao texto/botao

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { Button, Typography } from '@mui/material';
 
 function App() {
   const navigate = useNavigate(); // hook de navegação
@@ -8,9 +9,9 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Prova de React</h1>
-      <button onClick={handleClick}>Ver lista de Posts</button>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '16px', marginTop: '20px'}}>
+      <Typography variant="h4">Prova de React</Typography>
+      <Button variant="contained" color="primary" onClick={handleClick}>Ver lista de Posts</Button>
     </div>
   );
 }
