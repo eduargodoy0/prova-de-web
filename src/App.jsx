ddd2efd183f939758cd1878c8dc63047cdd5cb36
eduargodoy0 <eduargodoy22@gmail.com>
2025-06-08 11:44:19 -0300

    feat: alteração func

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([]); // Guarda os posts
+
+  useEffect(() => {
+    function getApi() {
+      // Faz a requisição GET com .then()
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => setPosts(data));
+    }
+
+    getApi(); // Chama a função ao iniciar
+  }, []);
 
   return (
-    <>
-    </>
-  )
+    <div>
+      <h1>Lista de Posts</h1>
+      {posts.map(post => (
+        <div key={post.id}>
+          <Link to={`/dados/${post.id}`}>{post.title}</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
