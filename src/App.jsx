70eddfb9f4e9fe3706ec36224508996bc973c459
eduargodoy0 <eduargodoy22@gmail.com>
2025-06-08 12:23:07 -0300

    refactor: mudança na navegação das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [posts, setPosts] = useState([]); // Guarda os posts
+  const [posts, setPosts] = useState([]); // guarda os posts
+  const navigate = useNavigate(); // hook para navegação
 
   useEffect(() => {
     function getApi() {
-      // Faz a requisição GET com .then()
+      // faz a requisição GET
       fetch('https://jsonplaceholder.typicode.com/posts')
         .then(res => res.json())
         .then(data => setPosts(data));
     }
 
-    getApi(); // Chama a função ao iniciar
+    getApi(); // chama a função
   }, []);
+
+  // função chamada ao clicar no post e navega para os dados do post
+  const handleClick = (id) => { 
+    navigate(`/dados/${id}`); // navega para rota com o id do post
+  };
 
   return (
     <div>
       <h1>Lista de Posts</h1>
       {posts.map(post => (
-        <div key={post.id}>
-          <Link to={`/dados/${post.id}`}>{post.title}</Link>
+        <div
+          key={post.id}
+          onClick={() => handleClick(post.id)} // Chama a função handleClick ao clicar
+        >
+          {post.title}
         </div>
       ))}
     </div>
