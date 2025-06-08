5b60f3ad581165c0fedf1dba37e3b786efa80474
eduargodoy0 <eduargodoy22@gmail.com>
2025-06-08 13:16:39 -0300

    feat: criação de botão para a rota posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [posts, setPosts] = useState([]); // guarda os posts
-  const navigate = useNavigate(); // hook para navegação
+  const navigate = useNavigate(); // hook de navegação
 
-  useEffect(() => {
-    function getApi() {
-      // faz a requisição GET
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => setPosts(data));
-    }
-
-    getApi(); // chama a função
-  }, []);
-
-  // função chamada ao clicar no post e navega para os dados do post
-  const handleClick = (id) => { 
-    navigate(`/dados/${id}`); // navega para rota com o id do post
+  const handleClick = () => {
+    navigate('/posts'); // navega para a rota /posts
   };
 
   return (
     <div>
-      <h1>Lista de Posts</h1>
-      {posts.map(post => (
-        <div
-          key={post.id}
-          onClick={() => handleClick(post.id)} // Chama a função handleClick ao clicar
-        >
-          {post.title}
-        </div>
-      ))}
+      <h1>Prova de React</h1>
+      <button onClick={handleClick}>Ver lista de Posts</button>
     </div>
   );
 }
