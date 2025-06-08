56a8a7c757b8989a2fde0e7f2381550424dd3187
eduargodoy0 <eduargodoy22@gmail.com>
2025-06-08 13:14:42 -0300

    style: alteração comentarios

--- src/pages/dados.jsx
+++ src/pages/dados.jsx
@@ -1,5 +1,5 @@
-import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Dados() {
   const { id } = useParams(); // pega o id da url
