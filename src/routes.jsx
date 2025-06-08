6d30a5cf956caad7d033c5241e3d7f682502d058
eduargodoy0 <eduargodoy22@gmail.com>
2025-06-08 13:17:37 -0300

    refactor: alteração de rota posts

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import App from './App';
 import Dados from './pages/dados.jsx';
+import Posts from './pages/posts.jsx';
 
 function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/posts" element={<Posts />} />
       <Route path="/dados/:id" element={<Dados />} />
     </Routes>
   );
