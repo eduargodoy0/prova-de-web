68f669b31e5c2cbeed41fcd2e16456e32eb5a2db
eduargodoy0 <eduargodoy22@gmail.com>
2025-06-08 12:22:37 -0300

    style: alteração de comentarios

--- src/pages/dados.jsx
+++ src/pages/dados.jsx
@@ -2,20 +2,22 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 function Dados() {
-  const { id } = useParams();
-  const [post, setPost] = useState(null);
+  const { id } = useParams(); // pega o id da url
+  const [post, setPost] = useState(null); // armazena o post
 
+  // quando a página abrir, busca os dados do post pelo id
   useEffect(() => {
     function getApi() {
-      // Faz a requisição GET com .then()
+      // faz a requisição GET
       fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(res => res.json())
         .then(data => setPost(data));
     }
 
-    getApi(); // Chama a função ao iniciar
+    getApi(); // chama a função quando carrega a página
   }, [id]);
 
+  // se ainda não carregou, mostra essa mensagem
   if (!post) 
     return <p>Carregando</p>;
 
